666b7d6ddbd609e0a63280b0c2a2827d5929c427
ChristopherAM08 <20233tn213@utez.edu.mx>
2025-03-15 16:32:53 -0600

    Se agrego diseño a Porfile y un boton de cerrar sesión

--- src/components/Porfile.jsx
+++ src/components/Porfile.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Porfile() {
   const userData = JSON.parse(localStorage.getItem("userData"));
+  const navigate = useNavigate();
+
+  const logOff = () => {
+    navigate("/login");
+  }
 
   return (
-    <div>
-      <h1>Prfil</h1>
-      <h4 style={{color:"black"}}>Datos del Usuario</h4>
-      <p>Nombre: {userData.fullName}</p>
-      <p>Apellidos: {userData.lastName}</p>
-      <p>Edad: {userData.age}</p>
-      <p>Teléfono: {userData.phone}</p>
-      <p>Correo: {userData.email}</p>
+    <div className="perfil-container">
+      <div className="card">
+        <h1>Perfil</h1>
+        <p style={{color:'black'}}>Datos del Usuario</p>
+        <p>Nombre: {userData.fullName}</p>
+        <p>Apellidos: {userData.lastName}</p>
+        <p>Edad: {userData.age}</p>
+        <p>Teléfono: {userData.phone}</p>
+        <p>Correo: {userData.email}</p>
+        
+        <button className="btn" onClick={logOff}>Cerrar Sesión</button>
+      </div>
     </div>
   );
 }
